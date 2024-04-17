8bb934740faa8256614d4dc5ad3c53c54d9fe657
SaiKalpana07 <saireddy7991@gmail.com>
2024-04-17 23:25:19 +0530

    Fetched filtered value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+
+const fruits = ["apple","mango","orange","strawberry","appy","mosambi","mangoes","appyFizz","applePie"];
+const newArray = []
+const[arrayList,setArrayList] = useState("")
+const[value,setValue] = useState("")
+
+  const handleChange = (event) => {
+  setArrayList(event.target.value)
+  setValue(event.target.value)
+  }
+
+  const filteredData = 
+    fruits.filter((f) => {
+       console.log(f,"filter")
+       if (f.includes(arrayList)) {
+        newArray.push(f)
+        console.log(f,"check")
+        console.log(arrayList,"checkArray") 
+      }
+    })
+    console.log(newArray,"new")
+  
   return (
     <div className="App">
       <h1>Click events</h1>
+      <input type="text" onChange={handleChange} value={value}/>
+      <h5>{newArray}</h5>
     </div>
   );
 }
